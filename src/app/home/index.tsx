844c0e057c7b98190843b82ae60c84cb3b0709fd
jkvim <jkvim@outlook.com>
2018-09-23 11:00:08 +0800

    feature: add home page

---
+++ src/app/home/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default class Home extends React.Component<any> {
+  render() {
+    return (
+      <div>Home</div>
+    )
+  }
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
-import { Login } from "./app/login";
+import Login from "./app/login";
 import NotFound from "./app/not-found";
+import Home from './app/home';
 
 export default class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Login path="/" />
+        <Home path="/" />
+        <Login path="/login" />
         <NotFound default />
       </Router>
     );
