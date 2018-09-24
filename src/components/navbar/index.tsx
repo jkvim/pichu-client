911d2f1bd622ac2200f683daad695c79feb1c56b
jkvim <jkvim@outlook.com>
2018-09-24 13:57:01 +0800

    feature: add MenuIcon for NavBar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,11 +6,18 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import { Link } from '@reach/router'
 
 const styles = {
   root: {
     flexGrow: 1,
+  },
+  navbar: {
+    paddingLeft: '150px',
+    paddingRight: '150px',
   },
   grow: {
     flexGrow: 1,
@@ -21,12 +28,32 @@ const styles = {
   },
 }
 
-class NavBar extends React.Component<any> {
+export interface NavBarState {
+  auth: Boolean;
+  anchorEl: any;
+}
+
+class NavBar extends React.Component<any, any> {
+  state: NavBarState = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleMenu = (event: any) => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  }
+
   render() {
     const { classes } = this.props
+    const { anchorEl } = this.state
+    const open = Boolean(anchorEl);
     return (
       <div className={classes.root}>
-        <AppBar position="static">
+        <AppBar position="static" className={classes.navbar}>
           <Toolbar>
             <IconButton
               className={classes.menuButton}
@@ -45,6 +72,33 @@ class NavBar extends React.Component<any> {
             <Button color="inherit">
               <Link to="/login">Login</Link>
             </Button>
+            <div>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
           </Toolbar>
         </AppBar>
       </div>
