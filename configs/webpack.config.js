0bbf11af8eed04b11d7569a0636fbac3edd57c45
jkvim <jkvim@outlook.com>
2018-09-24 10:03:07 +0800

    chore: add constants alias

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   resolve: {
     alias: {
       components: getPath('src/components/'),
+      constants: getPath('src/constants/')
     },
     extensions: ['.ts', '.tsx', '.js', '.json'],
   },
