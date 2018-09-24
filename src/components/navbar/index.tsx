55e33728c7fd4ee5a0077f1d2f6be359906d93ca
jkvim <jkvim@outlook.com>
2018-09-24 22:15:55 +0800

    feature: add Drawer to NavBar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,10 +6,18 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/Inbox'
 import { Link } from '@reach/router'
+import Categorys from 'constants/categorys'
+import values from 'lodash/values'
 
 const styles = {
   root: {
@@ -29,37 +37,65 @@ const styles = {
   logo: {
     textDecoration: 'none',
     color: 'white',
-  }
+  },
 }
 
 export interface NavBarState {
-  auth: Boolean;
-  anchorEl: any;
+  auth: boolean
+  anchorEl: any
+  isDrawerOpened: boolean
 }
 
 class NavBar extends React.Component<any, any> {
   state: NavBarState = {
     auth: true,
     anchorEl: null,
-  };
+    isDrawerOpened: false,
+  }
 
   handleMenu = (event: any) => {
-    this.setState({ anchorEl: event.currentTarget });
+    this.setState({ anchorEl: event.currentTarget })
   }
 
   handleClose = () => {
-    this.setState({ anchorEl: null });
+    this.setState({ anchorEl: null })
   }
+
+  handleOpenDrawer = () => {
+    this.setState({
+      isDrawerOpened: true,
+    })
+  }
+
+  handleCloseDrawer = () => {
+    this.setState({
+      isDrawerOpened: false,
+    })
+  }
+
+  categoryList = (
+    <List>
+      {values(Categorys).map(category => (
+        <ListItem>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary={category} />
+        </ListItem>
+      ))}
+    </List>
+  )
 
   render() {
     const { classes } = this.props
     const { anchorEl } = this.state
-    const open = Boolean(anchorEl);
+    const open = Boolean(anchorEl)
     return (
       <div className={classes.root}>
         <AppBar position="static" className={classes.navbar}>
           <Toolbar>
             <IconButton
+              onClick={this.handleOpenDrawer}
               className={classes.menuButton}
               color="inherit"
               aria-label="Menu"
@@ -68,10 +104,7 @@ class NavBar extends React.Component<any, any> {
             </IconButton>
             <div className={classes.grow}>
               <Link className={classes.logo} to="/">
-                <Typography
-                  variant="title"
-                  color="inherit"
-                >
+                <Typography variant="title" color="inherit">
                   Pichu
                 </Typography>
               </Link>
@@ -80,34 +113,42 @@ class NavBar extends React.Component<any, any> {
               <Link to="/login">Login</Link>
             </Button>
             <div>
-                <IconButton
-                  aria-owns={open ? 'menu-appbar' : null}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={this.handleClose}
-                >
-                  <Link to="/users/123">
-                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                  </Link>
-                </Menu>
-              </div>
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <Link to="/users/123">
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                </Link>
+              </Menu>
+            </div>
           </Toolbar>
+          <Drawer
+            anchor="left"
+            open={this.state.isDrawerOpened}
+            onClick={this.handleCloseDrawer}
+            onKeyDown={this.handleCloseDrawer}
+          >
+            {this.categoryList}
+          </Drawer>
         </AppBar>
       </div>
     )
---
+++ src/constants/categorys.ts
@@ -0,0 +1,8 @@
+export default {
+  ALL: '所有分类',
+  SOFTWARE_ENGINEERING: '软件工程',
+  PROGRAMMING_LANGUAGE: '编程语言',
+  ARTIFICIAL_INTELLIGENCE: '人工智能',
+  IOT: '硬件设备',
+  SOFTWARE_TESTING: '软件测试',
+}
