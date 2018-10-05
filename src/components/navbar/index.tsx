13e10ea6300690202fa71cee61533c7ab8040157
jkvim <jkvim@outlook.com>
2018-10-05 16:28:24 +0800

    feature: update style of Navbar button

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import { withStyles, createStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -19,7 +19,7 @@ import { Link } from '@reach/router'
 import Categorys from 'constants/categorys'
 import values from 'lodash/values'
 
-const styles = {
+const styles = () => createStyles({
   root: {
     flexGrow: 1,
   },
@@ -38,7 +38,11 @@ const styles = {
     textDecoration: 'none',
     color: 'white',
   },
-}
+  button: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
+})
 
 export interface NavBarState {
   auth: boolean
@@ -90,16 +94,10 @@ class NavBar extends React.Component<any, any> {
     const { classes } = this.props
     const { anchorEl } = this.state
     const open = Boolean(anchorEl)
-    return (
-      <div className={classes.root}>
+    return <div className={classes.root}>
         <AppBar position="static" className={classes.navbar}>
           <Toolbar>
-            <IconButton
-              onClick={this.handleOpenDrawer}
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
+            <IconButton onClick={this.handleOpenDrawer} className={classes.menuButton} color="inherit" aria-label="Menu">
               <MenuIcon />
             </IconButton>
             <div className={classes.grow}>
@@ -109,52 +107,28 @@ class NavBar extends React.Component<any, any> {
                 </Typography>
               </Link>
             </div>
-            <Button color="inherit">
-              <Link to="/login">登录</Link>
+            <Button component={props => <Link to="/login" {...props} />} className={classes.button}>
+              登录
             </Button>
-            <Button color="inherit">
-              <Link to="/register">注册</Link>
+            <Button component={props => <Link to="/register" {...props} />} className={classes.button}>
+              注册
             </Button>
             <div>
-              <IconButton
-                aria-owns={open ? 'menu-appbar' : null}
-                aria-haspopup="true"
-                onClick={this.handleMenu}
-                color="inherit"
-              >
+              <IconButton aria-owns={open ? 'menu-appbar' : null} aria-haspopup="true" onClick={this.handleMenu} color="inherit">
                 <AccountCircle />
               </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={this.handleClose}
-              >
+              <Menu id="menu-appbar" anchorEl={anchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={open} onClose={this.handleClose}>
                 <Link to="/users/123">
                   <MenuItem onClick={this.handleClose}>Profile</MenuItem>
                 </Link>
               </Menu>
             </div>
           </Toolbar>
-          <Drawer
-            anchor="left"
-            open={this.state.isDrawerOpened}
-            onClick={this.handleCloseDrawer}
-            onKeyDown={this.handleCloseDrawer}
-          >
+          <Drawer anchor="left" open={this.state.isDrawerOpened} onClick={this.handleCloseDrawer} onKeyDown={this.handleCloseDrawer}>
             {this.categoryList}
           </Drawer>
         </AppBar>
       </div>
-    )
   }
 }
 
