55c4f6f4b5b8e019f739f705f9689029e65bed6c
jkvim <jkvim@outlook.com>
2018-10-05 22:35:47 +0800

    feature: update style of button

--- src/components/topic-article/index.tsx
+++ src/components/topic-article/index.tsx
@@ -7,12 +7,13 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import LaunchIcon from '@material-ui/icons/Launch'
 import TurnedInIcon from '@material-ui/icons/TurnedIn'
 import ReplyIcon from '@material-ui/icons/Reply'
+import Button from '@material-ui/core/Button'
+import classnames from 'classnames'
 
 const styles = (theme: Theme) => createStyles({
   root: {
     display: 'flex',
-    borderBottom: '1px solid #e9e9e9',
-    paddingBottom: 10,
+    borderBottom: '1px solid #e9e9e9', paddingBottom: 10,
   },
   avatar: {
     margin: 10,
@@ -32,6 +33,22 @@ const styles = (theme: Theme) => createStyles({
     display: 'flex',
     justifyContent: 'flex-end',
     marginTop: 20,
+    alignItems: 'center',
+  },
+  reply: {
+    color: '#555'
+  },
+  actionButton: {
+    color: '#ccc',
+    '&:hover': {
+      color: '#000'
+    }
+  },
+  likeButton: {
+    '&:hover': {
+      color: '#f45048',
+      backgroundColor: '#fcdede'
+    }
   }
 })
 
@@ -50,10 +67,19 @@ class TopicSession extends React.Component<any, any> {
             {content}
           </div>
           <div className={classes.actions}>
-            <ThumbUpAltIcon />
-            <LaunchIcon />
-            <TurnedInIcon />
-            <span><ReplyIcon />回复</span>
+            <Button className={classnames(classes.actionButton, classes.likeButton)}>
+              <ThumbUpAltIcon />
+            </Button>
+            <Button className={classes.actionButton}>
+              <LaunchIcon />
+            </Button>
+            <Button className={classes.actionButton}>
+              <TurnedInIcon />
+            </Button>
+            <Button className={classnames(classes.actionButton, classes.reply)}>
+              <ReplyIcon />
+              回复
+            </Button>
           </div>
           {renderFooter && renderFooter()}
         </div>
